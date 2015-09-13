a097b728da01d204d94dc4dc225c906b5465ee24
Kartik Nyamagoudar <kartik.nyamagoudar@elotouch.com>
2015-09-12 18:39:24 -0700

    Process argv

--- program.js
+++ program.js
@@ -1,1 +1,10 @@
-console.log("HELLO WORLD");
+//console.log("HELLO WORLD");
+
+//console.log(process.argv);
+
+var numbers = process.argv;
+var sum =0;
+for (var i = 2 ; i < numbers.length ; i++) {
+	sum += +numbers[i]
+};
+console.log(sum);
